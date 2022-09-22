813a5ea1e150524e3bb8828b53b8d8200b5bf8e2
jack.zhang <823098037@qq.com>
2022-09-22 15:44:23 +0800

    resolve the cors

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
 // 可能的跨域解决方案
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   next();
-// });
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 //
 // var cors = require('cors');
 // app.use(cors());
